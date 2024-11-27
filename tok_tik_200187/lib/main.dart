77e14090fd4824b72b0e0bcb5b05cb516df4e25b
Hugosago <Hugo.sago30@gmail.com>
2024-11-27 12:44:57 -0600

    Practica24
    Muestra de los videos de la parte del archivo discoverprovider

--- tok_tik_200187/lib/main.dart
+++ tok_tik_200187/lib/main.dart
@@ -5,16 +5,19 @@ import 'package:tok_tik_200187/config/presentation/providers/discover_provider.d
 import 'package:tok_tik_200187/config/presentation/screens/discover_screens.dart';
 
 void main() => runApp(const MyApp());
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
-      providers: [ChangeNotifierProvider(create: (_) => DiscoverProvider())],
+      providers: [
+        ChangeNotifierProvider(
+          lazy: false,
+          create: (_) => DiscoverProvider()..loadNextPage())
+      ],
       child: MaterialApp(
-        title: 'TokTik 210077',
+        title: 'TokTik 200187',
         debugShowCheckedModeBanner: false,
         theme: AppTheme().getTheme(),
         home: const DiscoverScreens(),
